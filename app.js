de388b624b46783d4e74d0bb5a27ec8cd4d2eb56
Chakrit Wichian <chakrit@oozou.com>
2012-04-07 23:02:57 +0700

    Make cache time a variable so it's easy to adjust when debugging.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 (function() {
 
   var WEBROOT = './public'
-    , PORT = process.env.PORT || 8808;
+    , PORT = process.env.PORT || 8808
+    , OPTS = { cache: 3600 };
 
-  var files = new (require('node-static').Server)(WEBROOT);
+  var files = new (require('node-static').Server)(WEBROOT, OPTS);
 
-  console.log('Starting static file server on port ' + PORT + '...');
+  console.log('Listening on port ' + PORT + '...');
   require('http').createServer(function(req, resp) {
     req.addListener('end', function() {
       files.serve(req, resp);
